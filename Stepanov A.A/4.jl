f158874d9fb563095de1aa1ce8425fca204331af
KMBO-07 <76404272+KMBO-07@users.noreply.github.com>
2020-12-25 00:04:54 +0200

    Update 4.jl

--- Stepanov A.A/4.jl
+++ Stepanov A.A/4.jl
@@ -1,29 +1,29 @@
-function mark_treugol!(r::Robot)
-    num_vert = moves_end!(r, Sud)
-    num_hor = moves_end!(r, West)
-    h = 0
-    i = (moves_end!(r,Nord))
-    moves!(r,Sud)
-    j = (moves_end!(r,Ost) - 1)
-    moves!(r,West)
-    for k in 0:i
-        if k == 0
-            putmarkers!(r,Ost)
-            h+=1
-        elseif k % 2 == 1
-            putmarkers1!(r,West)
-            h+=1
-        elseif k % 2 == 0
-            putmarkers!(r,Ost,(j-k))
-            h+=1
+function mark_triangle!(r::Robot)
+    move_left_angle!(r)
+    lenght = moves!(r, Ost)
+    hight = moves!(r, Nord)
+    move_left_angle!(r)
+    j=lenght+1
+        for i in 1:hight
+            while j-i != 0
+                putmarker!(r)
+                move!(r,Ost)
+                j-=1
+            end
+            putmarker!(r)
+            j=lenght+1
+            back(r)
+            move!(r,Nord)
+        end
+end    
+function move_left_angle!(r::Robot)
+    for side in (Sud, West)
+        while isborder(r, side) == false
+            move!(r, side)
         end
     end
-    moves!(r, Sud)
-    moves!(r, West)
-    moves_end!(r, Nord, num_vert)
-    moves_end!(r, Ost, num_hor)    
 end
-function moves_end!(r::Robot,side::HorizonSide)
+function moves!(r::Robot,side::HorizonSide)
     num_steps=0
     while isborder(r,side)==false
         move!(r,side)
@@ -31,49 +31,8 @@ function moves_end!(r::Robot,side::HorizonSide)
     end
     return num_steps
 end
-function moves!(r::Robot,side::HorizonSide)
-    while isborder(r,side)==false
-        move!(r,side)
-    end
-end
-inverse(side::HorizonSide)=HorizonSide(mod(Int(side)+2,4))
-function putmarkers!(r::Robot,side::HorizonSide,i::Int)
-    for _ in 0:(i)
-        putmarker!(r)
-        move!(r,side)
-    end
-    putmarker!(r)
-    move!(r,side)
-    if (isborder(r,Nord)==false)
-    move!(r,Nord)
-    move!(r,West)
-    end
-end
-function putmarkers!(r::Robot,side::HorizonSide)
-    while isborder(r,side) == false
-        putmarker!(r)
-        move!(r,side)
-    end
-    putmarker!(r)
-    if isborder(r,Nord)==false
-        move!(r,Nord)
-    end
-end
-function putmarkers1!(r::Robot,side::HorizonSide)
-    if isborder(r,side)==false
-        move!(r,side)
-    end
-    while isborder(r,side)==false
-    putmarker!(r)
-    move!(r,side)
-    end
-    putmarker!(r)
-    if isborder(r,Nord)==false
-        move!(r,Nord)
-    end
-end
-function moves_end!(r::Robot,side::HorizonSide,num_steps::Int)
-    for _ in 1:num_steps 
-        move!(r,side)
+function go_back(r::Robot)
+    while isborder(r,West) == false
+        move!(r,West)
     end
 end
