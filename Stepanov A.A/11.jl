c864d4162d883642fd6d38437190b1c70f557957
KMBO-07 <76404272+KMBO-07@users.noreply.github.com>
2020-12-25 00:42:07 +0200

    Update 11.jl

--- Stepanov A.A/11.jl
+++ Stepanov A.A/11.jl
@@ -1,4 +1,4 @@
-unction mark_centers(r)
+function mark_centers(r)
     num_steps = through_rectangles_into_angle(r,(Sud,West))
     num_steps_to_ost = sum(num_steps[1:2:end])
     num_steps_to_nord = sum(num_steps[2:2:end])
